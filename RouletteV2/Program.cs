093f6890e065955b6b6c8fc902e56283b1d17e9a
d1m1tr11 <dimitri.kaxniashvili@gmail.com>
2024-09-20 14:42:45 +0400

    1

--- RouletteV2/Program.cs
+++ RouletteV2/Program.cs
@@ -1,8 +1,55 @@
-﻿Console.WriteLine("BID for Color or Number: ");
+﻿using RouletteV2;
+
+Console.WriteLine("BID for Color or Number: ");
 string userChoise = Console.ReadLine();
 
-if (userChoise != null && userChoise == "c" ) {
-    Console.WriteLine("you choose C");
-}
+bool userPlaying = true;
 
-else if
+Random rand = new();
+int randNumber = rand.Next(0, 37);
+
+string userBidColor;
+int userBidNumber;
+
+
+
+while (userPlaying)
+
+    if (userChoise == "c")
+    {
+        Console.WriteLine("enter Color R or B :");
+        userBidColor = Console.ReadLine();
+        if (userBidColor == "r")
+        {
+            Console.WriteLine("RED");
+            userChoise = null;
+        }
+        else if (userBidColor == "b")
+        {
+            Console.WriteLine("BLACK");
+            userChoise = null;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    else if (userChoise == "n")
+    {
+        Console.WriteLine("you choose N");
+        userChoise = null;
+    }
+
+    else
+    {
+        Console.WriteLine("Choose c, n or q for QUIT... ");
+        userChoise = Console.ReadLine();
+        if (userChoise == "q")
+        {
+            userPlaying = false;
+        }
+        else
+        {
+            userPlaying = true;
+        }
+    }

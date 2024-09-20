e7e590b3e831131ce8de720f9f34bf8f1897e900
Dimitri <dkakhniashvili@DKAKHNIASHVILI>
2024-09-20 13:38:54 +0400

    Add project files.

---
+++ RouletteV2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("BID for Color or Number: ");
+string userChoise = Console.ReadLine();
+
+if (userChoise != null && userChoise == "c" ) {
+    Console.WriteLine("you choose C");
+}
+
+else if
